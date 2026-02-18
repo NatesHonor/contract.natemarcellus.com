92832a441d352fd6076e96e0d92174635d8972cc
NatesHonor <nathaniel.j.marcellus@gmail.com>
2026-02-18 00:13:20 -0500

    Started contracting site

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,8 @@
+export default function DashboardLayout({ children }: { children: React.ReactNode}) {
+    return (
+        <div>
+            <div>DashBoard Layout </div>
+            <div>{children}</div>
+        </div>
+    )
+}
