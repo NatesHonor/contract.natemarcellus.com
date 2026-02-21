5c27ec3fce96ff3552ed2a50e6d8a4fcd5f29fa3
NatesHonor <nathaniel.j.marcellus@gmail.com>
2026-02-21 14:11:38 -0500

    Fixed sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.natemarcellus.com',
+  siteUrl: 'https://contract.natemarcellus.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
